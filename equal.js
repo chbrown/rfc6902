d0add16dd940dd90107b38fc635954e24573efb2
Christopher Brown <io@henrian.com>
2014-01-19 13:48:00 -0600

    Move equality testing from patch into own module

---
+++ equal.js
@@ -0,0 +1,63 @@
+'use strict'; /*jslint node: true, es5: true, indent: 2 */
+
+var _zip = function(a, b) {
+  var zipped = [];
+  for (var i = 0, l = Math.min(a.length, b.length); i < l; i++) {
+    zipped.push([a[i], b[i]]);
+  }
+  return zipped;
+};
+
+var _equalArrays = function(left, right) {
+  /** Assuming that left and right are both Arrays... */
+  if (left.length !== right.length) return false;
+
+  return _zip(left, right).every(function(pair) {
+    return equal(pair[0], pair[1]);
+  });
+};
+
+var _equalObjects = function(left, right) {
+  /** Assuming that left and right are both Objects... */
+  var left_keys = Object.keys(left);
+  var right_keys = Object.keys(right);
+  if (!_equalArrays(left_keys, right_keys)) return false;
+
+  return left_keys.every(function(key) {
+    return equal(left[key], right[key]);
+  });
+};
+
+var equal = module.exports = function(left, right) {
+  /**
+  > Here, "equal" means that the value at the target location and the
+  > value conveyed by "value" are of the same JSON type, and that they
+  > are considered equal by the following rules for that type:
+  > o  strings: are considered equal if they contain the same number of
+  >    Unicode characters and their code points are byte-by-byte equal.
+  > o  numbers: are considered equal if their values are numerically
+  >    equal.
+  > o  arrays: are considered equal if they contain the same number of
+  >    values, and if each value can be considered equal to the value at
+  >    the corresponding position in the other array, using this list of
+  >    type-specific rules.
+  > o  objects: are considered equal if they contain the same number of
+  >    members, and if each member can be considered equal to a member in
+  >    the other object, by comparing their keys (as strings) and their
+  >    values (using this list of type-specific rules).
+  > o  literals (false, true, and null): are considered equal if they are
+  >    the same.
+  */
+  // strict equality handles literals, numbers, and strings (a sufficient but not necessary cause)
+  if (left === right) return true;
+  // check arrays
+  if (Array.isArray(left) && Array.isArray(right)) {
+    return _equalArrays(left, right);
+  }
+  // check objects
+  if (Object(left) === left && Object(right) === right) {
+    return _equalObjects(left, right);
+  }
+  // mismatched arrays & objects, etc., are are all inequal
+  return false;
+};
--- patch.js
+++ patch.js
@@ -1,69 +1,8 @@
 'use strict'; /*jslint node: true, es5: true, indent: 2 */
 var pointer = require('./pointer');
 var errors = require('./errors');
+var equal = require('./equal');
 // var optimist = require('optimist');
-
-var _zip = function(a, b) {
-  var zipped = [];
-  for (var i = 0, l = Math.min(a.length, b.length); i < l; i++) {
-    zipped.push([a[i], b[i]]);
-  }
-  return zipped;
-};
-
-var _arrays_equal = function(left, right) {
-  /** Assuming that left and right are both Arrays... */
-  if (left.length !== right.length) return false;
-
-  return _zip(left, right).every(function(pair) {
-    return _equal(pair[0], pair[1]);
-  });
-};
-
-var _objects_equal = function(left, right) {
-  /** Assuming that left and right are both Objects... */
-  var left_keys = Object.keys(left);
-  var right_keys = Object.keys(right);
-  if (!_arrays_equal(left_keys, right_keys)) return false;
-
-  return left_keys.every(function(key) {
-    return _equal(left[key], right[key]);
-  });
-};
-
-var _equal = function(left, right) {
-  /**
-  > Here, "equal" means that the value at the target location and the
-  > value conveyed by "value" are of the same JSON type, and that they
-  > are considered equal by the following rules for that type:
-  > o  strings: are considered equal if they contain the same number of
-  >    Unicode characters and their code points are byte-by-byte equal.
-  > o  numbers: are considered equal if their values are numerically
-  >    equal.
-  > o  arrays: are considered equal if they contain the same number of
-  >    values, and if each value can be considered equal to the value at
-  >    the corresponding position in the other array, using this list of
-  >    type-specific rules.
-  > o  objects: are considered equal if they contain the same number of
-  >    members, and if each member can be considered equal to a member in
-  >    the other object, by comparing their keys (as strings) and their
-  >    values (using this list of type-specific rules).
-  > o  literals (false, true, and null): are considered equal if they are
-  >    the same.
-  */
-  // strict equality handles literals, numbers, and strings (a sufficient but not necessary cause)
-  if (left === right) return true;
-  // check arrays
-  if (Array.isArray(left) && Array.isArray(right)) {
-    return _arrays_equal(left, right);
-  }
-  // check objects
-  if (Object(left) === left && Object(right) === right) {
-    return _objects_equal(left, right);
-  }
-  // mismatched arrays & objects, etc., are are all inequal
-  return false;
-};
 
 var _add = function(object, key, value) {
   if (Array.isArray(object)) {
@@ -196,7 +135,7 @@ var ops = {
     */
     var endpoint = pointer.at(object, operation.path);
     // endpoint.value
-    var result = _equal(endpoint.value, operation.value);
+    var result = equal(endpoint.value, operation.value);
     if (!result) return new errors.TestError(endpoint.value, operation.value);
   }
 };
