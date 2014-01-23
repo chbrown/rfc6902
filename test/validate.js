3d5115cbdc649ca370b8a29445963528625d4a0e
Christopher Brown <io@henrian.com>
2014-01-22 21:59:56 -0600

    Fix a few tests, due to that diffable property

--- test/validate.js
+++ test/validate.js
@@ -12,7 +12,7 @@ tap.test('proper spec', function(t) {
     var spec = yaml.load(data);
 
     // use sorted values and sort() to emulate set equality
-    var props = ['errors', 'input', 'name', 'output', 'patch'];
+    var props = ['diffable', 'errors', 'input', 'name', 'output', 'patch'];
     var props_csv = props.join(', ');
     spec.forEach(function(item) {
       t.deepEqual(Object.keys(item).sort(), props, 'each spec item should have properties: ' + props_csv);
