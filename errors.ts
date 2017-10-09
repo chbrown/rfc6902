48b99e518f11d8c3e6f780e5920d654de0dce90d
Christopher Brown <io@henrian.com>
2017-10-09 13:26:58 -0500

    Set Error subclass names literally to avoid reflection issues

--- errors.ts
+++ errors.ts
@@ -1,21 +1,21 @@
 export class MissingError extends Error {
   constructor(public path: string) {
     super(`Value required at path: ${path}`);
-    this.name = this.constructor.name;
+    this.name = 'MissingError';
   }
 }
 
 export class InvalidOperationError extends Error {
   constructor(public op: string) {
     super(`Invalid operation: ${op}`);
-    this.name = this.constructor.name;
+    this.name = 'InvalidOperationError';
   }
 }
 
 export class TestError extends Error {
   constructor(public actual: any, public expected: any) {
     super(`Test failed: ${actual} != ${expected}`);
-    this.name = this.constructor.name;
+    this.name = 'TestError';
     this.actual = actual;
     this.expected = expected;
   }
